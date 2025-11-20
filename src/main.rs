99535a3a0beb7c7bfeb9d489e4b4f4946664b09b
Szybet <53944559+Szybet@users.noreply.github.com>
2025-11-20 14:08:13 +0000

    Make bridges optional

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use anyhow::Result;
 use tokio::{signal, sync::mpsc};
 
+#[cfg(feature = "bridges")]
 pub mod bridge {
     use tokio::sync::mpsc;
 
     use crate::ebc;
 
+    #[cfg(feature = "sway")]
     pub mod sway;
 
     pub async fn start(tx: mpsc::Sender<ebc::Command>) -> Option<String> {
+        #[cfg(feature = "sway")]
         let res = sway::start(tx.clone()).await;
+
+        // Add here other bridges with AND for the check to work
+        #[cfg(not(feature = "sway"))]
+        {
+            compile_error!(
+                "bridges feature is enabled but no specific bridges are enabled, this is wrong, enable sway feature for example"
+            );
+        }
 
         match res {
             Ok(s) => Some(s),
@@ -39,7 +50,10 @@ async fn main() -> Result<()> {
         ebc.serve(rx).await;
     });
 
+    #[cfg(feature = "bridges")]
     let selected_bridge = bridge::start(tx.clone()).await.unwrap_or_default();
+    #[cfg(not(feature = "bridges"))]
+    let selected_bridge = String::new();
 
     let _dbus_ctx = dbus::Context::initialize(tx.clone(), selected_bridge).await?;
 
